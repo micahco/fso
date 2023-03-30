75b643ec1f7230497eed3b4f7c10e8fa1cf39c65
micah <micahcowell@gmail.com>
2023-03-29 23:46:33 -0700

    part2_1-5

---
+++ part2/courseinfo2/src/components/Course.js
@@ -0,0 +1,23 @@
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part key={part.id} part={part} />)}     
+  </>
+
+const Total = ({ parts }) =>
+  <b>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b>
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
