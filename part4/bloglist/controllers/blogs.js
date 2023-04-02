bd592a8cf3e1380d8ef1c1c144e35d80cd92c519
micah <micahcowell@gmail.com>
2023-04-02 01:40:54 -0700

    4.12

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,35 +1,26 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(notes => {
-    response.json(notes)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
   })
-
-  blog.save()
-    .then(savedBlog => {
-      response.json(savedBlog)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,117 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const Blog = require('../models/blog')
+const helper = require('./blog_helper')
+const app = require('../app')
+const api = supertest(app)
+const timeout = 10000
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+}, timeout)
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+  const blogTitlesAtEnd = response.body.map(blog => blog.title)
+  expect(blogTitlesAtEnd).toContain(helper.initialBlogs[0].title)
+})
+
+test('blogs are defined', async () => {
+  const blogs = await helper.blogsInDb()
+  expect(blogs[0]).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'A Complete Guide to useEffect',
+    author: 'Dan Abramov',
+    url: 'https://overreacted.io/a-complete-guide-to-useeffect/',
+    likes: 8
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  const blogTitlesAtEnd = blogsAtEnd.map(blog => blog.title)
+  expect(blogTitlesAtEnd).toContain(newBlog.title)
+})
+
+test('a blog can be added without likes and the likes default to 0', async () => {
+  const newBlog = {
+    title: 'A Complete Guide to useEffect',
+    author: 'Dan Abramov',
+    url: 'https://overreacted.io/a-complete-guide-to-useeffect/'
+  }
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  expect(response.body.likes).toBe(0)
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+})
+
+test('a blog without title is not added', async () => {
+  const newBlog = {
+    author: 'Dan Abramov',
+    url: 'https://overreacted.io/a-complete-guide-to-useeffect/',
+    likes: 8
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
+test('a blog without url is not added', async () => {
+  const newBlog = {
+    title: 'A Complete Guide to useEffect',
+    author: 'Dan Abramov',
+    likes: 8
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+  const titles = blogsAtEnd.map(r => r.title)
+  expect(titles).not.toContain(blogToDelete.title)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
